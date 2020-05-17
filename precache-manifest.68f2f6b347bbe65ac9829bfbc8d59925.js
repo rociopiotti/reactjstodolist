b0d460a6744d8a8b814cb580e0d0ff42dddb6fd8
Rocio <ropiotti@gmail.com>
2020-05-17 16:13:43 +0200

    Updates

---
+++ precache-manifest.68f2f6b347bbe65ac9829bfbc8d59925.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20ee0b1a45f588d5407cc8a6ca7efeda",
+    "url": "/public/webdesign/todolist/index.html"
+  },
+  {
+    "revision": "8f7a8b3b6938b6711d10",
+    "url": "/public/webdesign/todolist/static/css/main.cb3bc825.chunk.css"
+  },
+  {
+    "revision": "e01f4c6a7711f1df8d21",
+    "url": "/public/webdesign/todolist/static/js/2.b42cb84a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/public/webdesign/todolist/static/js/2.b42cb84a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f7a8b3b6938b6711d10",
+    "url": "/public/webdesign/todolist/static/js/main.e8cd54fb.chunk.js"
+  },
+  {
+    "revision": "6e89c5abbad70c1632b9",
+    "url": "/public/webdesign/todolist/static/js/runtime-main.d8da4b60.js"
+  }
+]);
